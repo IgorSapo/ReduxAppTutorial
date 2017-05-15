f0cb747c59e507fb24c47fb606e7b86393f2d5a6
IgorS <amsterdam1611@gmail.com>
2017-05-15 21:04:03 +0400

    Lesson 15

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
 import { v4 } from 'uuid';
 
+export const recieveTodos = (filter, response) => ({
+  type: 'RECIEVE_TODOS',
+  filter,
+  response,
+})
+
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
   id: v4(),
--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const isActive = (match, location) => {
-  window.console.log('Match is: ');
-  window.console.log(match);
-  window.console.log('Location is: ');
-  window.console.log(location);
-  window.console.log("------------");
-  return match;
-};
-
 const FilterLink = ({ filter, children }) => (
   <NavLink
     exact
@@ -18,7 +9,6 @@ const FilterLink = ({ filter, children }) => (
       textDecoration: 'none',
       color: 'black',
     }}
-    isActive={isActive}
   >
     {children}
   </NavLink>
